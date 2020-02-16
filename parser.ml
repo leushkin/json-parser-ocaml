c39efed3aff02829b9b421cb5b2bb77ffa729a6c
leushkin <kirill.leushkin1@gmail.com>
2020-02-16 14:02:26 +0300

    key/value parsing

--- parser.ml
+++ parser.ml
@@ -36,6 +36,34 @@ type t =
 
 let explode str = List.init (String.length str) (String.get str)
 
+let to_string chars = String.concat "" (List.map (String.make 1) chars)
+
+let find_index_right l item =
+  let index = ref None in
+  for i = List.length l - 1 downto 0 do
+    if List.nth l i = item && !index = None
+    then index := Some i
+  done;
+  !index;;
+
+let take_while l p =
+  let output = ref [] in
+  let flag = ref false in
+  for i = 0 to List.length l - 1 do
+    if (not (p (List.nth l i))) && not !flag
+    then output := !output @ [List.nth l i]
+    else flag := true
+  done;
+  !output;;
+
+let drop l c =
+  let output = ref [] in
+  for i = 0 to List.length l - 1 do
+    if i >= c
+    then output := !output @ [List.nth l i]
+  done;
+  !output;;
+
 let drop_spaces str =
   let flag = ref false in
   let check_char = function
@@ -47,9 +75,11 @@ let drop_spaces str =
 
 let uncons l = (List.hd l, List.tl l)
 
-let rec json_to_tokens = function
-  | []      -> []
-  | x :: xs ->
+let rec json_to_tokens tokens = function
+  | []      -> tokens
+  | xs ->
+    let x = List.hd xs in
+    let xs = ref (List.tl xs) in
     let token = match x with
       | '{' -> LEFT_BRACKET
       | '}' -> RIGHT_BRACKET
@@ -57,37 +87,29 @@ let rec json_to_tokens = function
       | ']' -> RIGHT_BRACE
       | ',' -> COMMA
       | ':' -> COLON
+      | '"' ->
+        let closestCommaIndex = find_index_right tokens COMMA in
+        let closestColonIndex = find_index_right tokens COLON in
+        let str = take_while !xs ((=)'"') in
+        xs := drop !xs (List.length str + 1);
+        if closestColonIndex > closestCommaIndex
+        then STRING (to_string str)
+        else KEY (to_string str)
       | _ -> NULL
     in
-    token :: json_to_tokens xs
-
-  (* | '[' -> LEFT_BRACE
-  | ']' -> RIGHT_BRACE
-  | ',' -> COMMA
-  | ':' -> COLON *)
-
-(* 
-  | NULL
-  | TRUE
-  | FALSE
-  | STRING of string
-  | INTEGER of int
-  | FLOAT of float
-  | KEY of string
-  | COLON
-  | EOF *)
+    json_to_tokens (tokens @ [token]) !xs;;
 
 let parse_json str = str
   |> explode
   |> drop_spaces
-  |> json_to_tokens
+  |> json_to_tokens []
 
 let print_tokens l = List.map token_to_string l
   |> List.map (fun x -> x ^ ", ")
   |> List.fold_left (^) ""
   |> print_string
 
-let example = "{ \"key\": \"value\" }"
+let example = "{ \"key\": \"value\", \"key\": \"value\" }"
 
 let read () =
   print_string "> ";
